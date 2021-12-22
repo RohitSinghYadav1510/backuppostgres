959116f64d60f99935908efff37097534005e46c
Rohit Singh Yadav <47585976+RohitSinghYadav1510@users.noreply.github.com>
2021-12-22 15:30:57 +0530

    add python code

---
+++ backup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import os
+from subprocess import PIPE,Popen
+
+requirement = 'only database'
+host     = 'localhost'
+database = 'postgres'
+user     = 'postgres'
+schema   = 'public'
+table    = 'company'
+password = 'example'
+
+
+if 'only' in requirement and 'database' in requirement:
+    command = 'pg_dump -h {0} -U {2} -p 5432 -d {1} -Fc -f pg_{1}.dump'.format(host,database,user,schema)
+
+elif 'schema' in requirement and 'all' in requirement:
+    command = 'pg_dump -h {0} -U {2} -p 5432 -d {1} -n {1}.* -Fc -f pg_allschemas.dump'.format(host,database,user)
+
+elif 'only' in requirement and 'schema' in requirement:
+    command = 'pg_dump -h {0} -U {2} -p 5432 -d {1} -n {3} -Fc -f pg_{3}_schema.dump'.format(host,database,user,schema)
+
+elif 'table' in requirement and 'all' in requirement:
+    command = 'pg_dump -h {0} -d {1} -U {2} -p 5432 -t {3}.* -Fc -f pg_{3}_allTable.dump'.format(host,database,user,schema)
+
+elif 'only' in requirement and 'table' in requirement:
+    command = 'pg_dump -h {0} -d {1} -U {2} -p 5432 -t {3}.{4} -Fc -f pg_{3}_{4}.dump'.format(host,database,user,schema,table)
+
+else:
+    print("requirement is not match")
+
+
+
+p = Popen(command, shell=True, env={**os.environ, "PGPASSWORD": password})
+p.communicate('{}\n'.format(password))
