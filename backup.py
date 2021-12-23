127ff05c927cafc5cd884ac90a755a5081c75f5a
Rohit Singh Yadav <47585976+RohitSinghYadav1510@users.noreply.github.com>
2021-12-23 16:37:45 +0530

    Update backup.py

--- backup.py
+++ backup.py
@@ -12,7 +12,7 @@ user        = os.getenv("user_name")
 password    = os.getenv("database_passwd")
 schema      = os.getenv("schema_name")
 table       = os.getenv("table_name")
-#bucketname  = os.getenv("bucket_name")
+bucketname  = os.getenv("bucket_name")
 
 os.system("rm pg*")
 
@@ -24,32 +24,22 @@ def runprocess(password):
 if 'only' in requirement and 'database' in requirement:
     command = 'pg_dump -h {0} -U {2} -p 5432 -d {1} -Fc -f pg_{1}.dump'.format(host,database,user,schema)
     runprocess(password)
-    #p = Popen(command, shell=True, env={**os.environ, "PGPASSWORD": password})
-    #p.communicate('{}\n'.format(password))
     
 elif 'schema' in requirement and 'all' in requirement:
     command = 'pg_dump -h {0} -U {2} -p 5432 -d {1} -n {1}.* -Fc -f pg_allschemas.dump'.format(host,database,user)
     runprocess(password)
-    #p = Popen(command, shell=True, env={**os.environ, "PGPASSWORD": password})
-    #p.communicate('{}\n'.format(password))
     
 elif 'only' in requirement and 'schema' in requirement:
     command = 'pg_dump -h {0} -U {2} -p 5432 -d {1} -n {3} -Fc -f pg_{3}_schema.dump'.format(host,database,user,schema)
     runprocess(password)
-    #p = Popen(command, shell=True, env={**os.environ, "PGPASSWORD": password})
-    #p.communicate('{}\n'.format(password))
     
 elif 'table' in requirement and 'all' in requirement:
     command = 'pg_dump -h {0} -d {1} -U {2} -p 5432 -t {3}.* -Fc -f pg_{3}_allTable.dump'.format(host,database,user,schema)
     runprocess(password)
-    #p = Popen(command, shell=True, env={**os.environ, "PGPASSWORD": password})
-    #p.communicate('{}\n'.format(password))
     
 elif 'only' in requirement and 'table' in requirement:
     command = 'pg_dump -h {0} -d {1} -U {2} -p 5432 -t {3}.{4} -Fc -f pg_{3}_{4}.dump'.format(host,database,user,schema,table)
-    runprocess(password)  
-    #p = Popen(command, shell=True, env={**os.environ, "PGPASSWORD": password})
-    #p.communicate('{}\n'.format(password))
+    runprocess(password)
 
 else:
     print("requirement is not match")
