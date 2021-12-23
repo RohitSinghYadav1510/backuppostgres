e238acb5c2a3fc2c3406f4eeb4de4056728cf8e8
Rohit Singh Yadav <47585976+RohitSinghYadav1510@users.noreply.github.com>
2021-12-23 13:19:47 +0530

    Update backup.py

--- backup.py
+++ backup.py
@@ -21,7 +21,9 @@ def runprocess(password):
 
 if 'only' in requirement and 'database' in requirement:
     command = 'pg_dump -h {0} -U {2} -p 5432 -d {1} -Fc -f pg_{1}.dump'.format(host,database,user,schema)
-    runprocess(password)
+    #runprocess(password)
+    p = Popen(command, shell=True, env={**os.environ, "PGPASSWORD": password})
+    p.communicate('{}\n'.format(password))
     
 elif 'schema' in requirement and 'all' in requirement:
     command = 'pg_dump -h {0} -U {2} -p 5432 -d {1} -n {1}.* -Fc -f pg_allschemas.dump'.format(host,database,user)
