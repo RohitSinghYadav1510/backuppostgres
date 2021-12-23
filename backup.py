6b3a19f1cafd13a90bd65007c3494f388fc2d0b7
Rohit Singh Yadav <47585976+RohitSinghYadav1510@users.noreply.github.com>
2021-12-23 13:16:04 +0530

    Update backup.py

--- backup.py
+++ backup.py
@@ -14,30 +14,30 @@ bucketname  = os.getenv("bucket_name")
 schema      = os.getenv("schema_name")
 table       = os.getenv("table_name")
 
-def run(password):
+def runprocess(password):
     p = Popen(command, shell=True, env={**os.environ, "PGPASSWORD": password})
     p.communicate('{}\n'.format(password))
   
 
 if 'only' in requirement and 'database' in requirement:
     command = 'pg_dump -h {0} -U {2} -p 5432 -d {1} -Fc -f pg_{1}.dump'.format(host,database,user,schema)
-    run(password)
+    runprocess(password)
     
 elif 'schema' in requirement and 'all' in requirement:
     command = 'pg_dump -h {0} -U {2} -p 5432 -d {1} -n {1}.* -Fc -f pg_allschemas.dump'.format(host,database,user)
-    run(password)
+    runprocess(password)
     
 elif 'only' in requirement and 'schema' in requirement:
     command = 'pg_dump -h {0} -U {2} -p 5432 -d {1} -n {3} -Fc -f pg_{3}_schema.dump'.format(host,database,user,schema)
-    run(password)
+    runprocess(password)
     
 elif 'table' in requirement and 'all' in requirement:
     command = 'pg_dump -h {0} -d {1} -U {2} -p 5432 -t {3}.* -Fc -f pg_{3}_allTable.dump'.format(host,database,user,schema)
-    run(password)
+    runprocess(password)
     
 elif 'only' in requirement and 'table' in requirement:
     command = 'pg_dump -h {0} -d {1} -U {2} -p 5432 -t {3}.{4} -Fc -f pg_{3}_{4}.dump'.format(host,database,user,schema,table)
-    run(password)   
+    runprocess(password)   
 
 else:
     print("requirement is not match")
